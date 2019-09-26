e7a47cc3a95a68e539789dcaa623fce1bed187c9
ginakui <developerkui@outlook.com>
2019-09-26 15:52:15 -0400

    refactor Prices with react useState hook

--- components/Prices.js
+++ components/Prices.js
@@ -1,32 +1,27 @@
 /*
 *  this Display the bitcoin price
 */
-class Prices extends React.Component {
-  state = {
-    currency: 'EUR'
-  }
-
-  render() {
-    const { description, code, rate } = this.props.bpi[this.state.currency];  
-    return (
-      <div>
-        <ul className="list-group">
-          <li className="list-group-item">
-            Bitcoin rate for {description}
-            : <span className="badge badge-primary">{code}</span>
-            <strong>{rate}</strong>
-          </li>
-        </ul>
-        <hr />
-        <select className="form-control" onChange={e => this.setState({currency: e.target.value})}>
-          
-          <option value="USD" selected>USD</option>
-          <option value="GBP">GBP</option>
-          <option value="EUR">EUR</option>
-        </select>
-      </div>
-    );
-  }
+const Prices = (props) => {
+  const [currency, setCurrency] = React.useState('USD');
+  const { description, code, rate } = props.bpi[currency];
+  
+  return (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">
+          Bitcoin rate for {description}
+          : <span className="badge badge-primary">{code}</span>
+          <strong>{rate}</strong>
+        </li>
+      </ul>
+      <hr />
+      <select className="form-control" onChange={e => setCurrency(e.target.value)}>   
+        <option value="USD" selected>USD</option>
+        <option value="GBP">GBP</option>
+        <option value="EUR">EUR</option>
+      </select>
+    </div>
+  );
 }
 
 export default Prices;
